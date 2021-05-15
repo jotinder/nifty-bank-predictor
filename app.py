174d2d1792e5f0b13df09d40825a496b5eb3dbdd
Jotinder Singh <jotmatta@gmail.com>
2021-05-16 00:28:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,27 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
- # Install requests package for python
- import requests
-
- # Set the request parameters
- url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=RELIANCE.BSE&outputsize=full&apikey=demo"'
-
- # Set the user credentials
- #user = 'username'
- #pwd = 'password'
-
- # Set the proper headers
- headers = {"Accept":"application/json"}
-
- # Make the HTTP request
- #response = requests.get(url, auth=(user, pwd), headers=headers)
- response = requests.get(url,headers=headers)
-
- # Check for HTTP codes other than 200
- if response.status_code != 200: 
-     print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.content)
-     exit()
-
- # Decode the XML response into a dictionary and use the data
- print(response.content)
+    return "Hello, World!"
